31a6b1b467373fd66e1a7e9004b568c35e9d1c26
gbdngb12 <gbdngb12@naver.com>
2023-01-25 04:23:39 +0000

    build executables & install executables

---
+++ 2-1/build/CMakeFiles/dong.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dong.dir/main.cpp.o"
+  "CMakeFiles/dong.dir/main.cpp.o.d"
+  "dong"
+  "dong.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dong.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
