5605c3ffe73b75775833ebdddc6b86b50780f05d
gbdngb12 <gbdngb12@naver.com>
2023-01-27 03:32:56 +0000

    git add submodule

---
+++ 3-1/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main() {
+    GLFWwindow *window;
+    if(!glfwInit()) {
+        fprintf(stderr, "Failed to initalize GLFW\n");
+        exit(EXIT_FAILURE);
+    }
+
+    glfwWindowHint(GLFW_DEPTH_BITS, 16);
+    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+    // Main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+    return 0;
+}
